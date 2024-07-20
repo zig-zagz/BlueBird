3313ae12069e04a67de9abcebf3f38b95e1b01f8
jackz <z1ggy0z4ggy@gmail.com>
2024-07-20 15:28:59 +0100

    update

--- app/global.d.ts
+++ app/global.d.ts
@@ -1,6 +1,16 @@
 import { Database as DB } from "@/lib/database.types";
 
+
+type Tweet = DB['public']['Tables']['tweets']['Row']
+type Profile = DB['public']['Tables']['profiles']['Row']
+
+
 //Create a global type for the Database so that we don't have to import it everywhere
 declare global {
     type Database = DB;
+    type TweetWithAuthor = Tweet & {
+        author: Profile;
+        likes: number;
+        user_has_liked_tweet: boolean;  
+      }
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,41 @@
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
 import AuthButtonServer from "./auth-button-server";
+import { redirect } from "next/navigation";
 import NewTweet from "./new-tweet";
-
+import Likes from "./likes";
+import Tweets from "./tweets";
 
 export default async function Home() {
   const supabase = createServerComponentClient<Database>({ cookies });
 
-  const {data: {session}} = await supabase.auth.getSession();
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
 
   if (!session) {
-    redirect('/login')
+    redirect("/login");
   }
 
-  const { data: tweets } = await supabase.from("tweets").select("*, profiles(*)");
+  const { data } = await supabase
+    .from("tweets")
+    .select("*, author: profiles(*), likes(user_id)");
+
+  const tweets =
+    data?.map((tweet) => ({
+      ...tweet,
+      author: Array.isArray(tweet.author) ? tweet.author[0] : tweet.author,
+      user_has_liked_tweet: !!tweet.likes.find(
+        (like) => like.user_id === session.user.id
+      ),
+      likes: tweet.likes.length,
+    })) ?? [];
 
   return (
     <>
       <AuthButtonServer />
       <NewTweet />
-      {tweets?.map((tweet) => (
-        <div key={tweet.id}>
-          <p> {tweet?.profiles?.name} {tweet?.profiles?.username}</p>
-          <p> {tweet.title} </p>
-        </div>
-      ))}
+      <Tweets tweets={tweets} />
     </>
   );
 }
